bc2003eba896e43c637fba96489a077faeeaac05
= <=thanicha_mongkorn@cmu.ac.th>
2023-03-03 14:51:56 +0700

    name

--- average.cpp
+++ average.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+using namespace std;
 
+int main(int argc,char* argv[]){
+    int sum=0;
+    for(int i=0;i<argc;i++){
+        char* text =argv[i];
+        sum=text+sum;
+    }
+    cout <<sum/argc;
+
+    return 0;
+}
