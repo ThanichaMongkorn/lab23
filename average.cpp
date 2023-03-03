ee4a82fae2605b313f7b623734d12c444c2898ca
= <=thanicha_mongkorn@cmu.ac.th>
2023-03-03 15:24:13 +0700

    average

--- average.cpp
+++ average.cpp
@@ -1,13 +1,24 @@
 #include<iostream>
+#include<string>
+#include<cstdlib>
 using namespace std;
 
 int main(int argc,char* argv[]){
-    int sum=0;
-    for(int i=0;i<argc;i++){
-        char* text =argv[i];
-        sum=text+sum;
+    float sum=0;
+    float number;
+    if(argc==1){
+        cout <<"Please input numbers to find average.";
+    }else{
+        cout <<"---------------------------------\n";
+            for(int i=1;i<argc;i++){
+            char* text =argv[i];
+            number=atof(text);
+            sum = number+sum;
+        }
+        cout <<"Average of "<<argc-1<<" numbers = "<<sum/(argc-1)<<"\n";
+        cout <<"---------------------------------\n";
+        
+        
     }
-    cout <<sum/argc;
-
     return 0;
 }
